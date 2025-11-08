62cfb34e2a951ae181b4a2b8dbe31f48923b3bb5
GabeOliveira20 <gabriel.oliveira12500@gmail.com>
2025-11-08 16:08:46 -0300

    Refresh

--- Jogo.py
+++ Jogo.py
@@ -1,3 +1,4 @@
+import random
 # Personagem classe mãe
 # Heroi controlado pelo usuario
 #inimigo adversario do usuario
@@ -26,7 +27,7 @@ class Personagem:
             self.__vida = 0
     
     def atacar(self, alvo):
-        dano = self.get_nivel() * 2
+        dano = random.randint(self.get_nivel() * 2, self.get_nivel() * 4) # Baseado no nivel do personagem
         alvo.receber_ataque(dano)
         print(f"{self.get_nome()} atacou {alvo.get_nome()} e causou {dano} de dano!")
 
@@ -52,11 +53,17 @@ class Inimigo(Personagem):
     def exibir_detalhes(self):
         return f"{super().exibir_detalhes()}\nTipo: {self.get_tipo()}\n"
 
+    def ataque_especial(self, alvo):
+        dano = random.randint(self.get_nivel() * 5, self.get_nivel() * 8) # Dano aumentado
+        alvo.receber_ataque(dano)
+        print(f"{self.get_nome()} usou a habilidade especial {self.get_habilidade()} em {alvo.get_nome()} e causou {dano} de dano!")
+
+ 
 class Jogo:
     """Classe principal do jogo"""
     def __init__(self):
         self.heroi = heroi = Heroi("Heroi", vida=100, nivel=5, habilidade="Super força")
-        self.inimigo = inimigo = Inimigo("Morcego", vida=50, nivel=3, tipo="Voador")
+        self.inimigo = inimigo = Inimigo("Morcego", vida=50, nivel=5, tipo="Voador")
 
     def iniciar_batalha(self):
         print("Batalha iniciada!")
@@ -70,8 +77,15 @@ class Jogo:
 
             if escolha == '1':
                 self.heroi.atacar(self.inimigo)
+            elif escolha == '2':
+                self.heroi.ataque_especial(self.inimigo)
             else:
                 print("Escolha inválida")
+            if self.inimigo.get_vida() >= 0:
+                # Inimigo ataca o heroi
+                self.inimigo.atacar(self.heroi)
+
+
         if self.heroi.get_vida() > 0:
             print("Herói venceu a batalha!")
         else:
